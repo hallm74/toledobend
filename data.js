3cdd9061ba12bd6fa7dd75428a8740ff26c31d68
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-03 17:45:45 -0600

    added ogimage

--- data.js
+++ data.js
@@ -1,27 +1,14 @@
 const data = {
-<<<<<<< HEAD
   "lakeLevel": "169.74 ft",
-=======
-  "lakeLevel": "Unavailable",
->>>>>>> Toledo-Bend/main
   "weather": {
     "temp": 61.12,
     "feels_like": 60.37,
     "description": "clear sky",
-<<<<<<< HEAD
     "icon": "https://openweathermap.org/img/wn/01n@2x.png",
     "wind_speed": 4.32,
     "wind_deg": 52,
     "gust": 8.93,
     "sunrise": "01:13 AM",
     "sunset": "11:22 AM"
-=======
-    "icon": "https://openweathermap.org/img/wn/01d@2x.png",
-    "wind_speed": 4.32,
-    "wind_deg": 52,
-    "gust": 8.93,
-    "sunrise": "07:13 AM",
-    "sunset": "05:22 PM"
->>>>>>> Toledo-Bend/main
   }
 };
