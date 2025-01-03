66b44b741d423e646d09d0ffcf436e42726bf4b4
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-03 17:36:45 -0600

    changed to bootstrap dynamic weather icon

--- data.js
+++ data.js
@@ -1,19 +1,14 @@
 const data = {
-  "lakeLevel": "169.71 ft",
+  "lakeLevel": "169.74 ft",
   "weather": {
-    "temp": 66.52,
-    "feels_like": 65.37,
+    "temp": 61.12,
+    "feels_like": 60.37,
     "description": "clear sky",
-    "icon": "https://openweathermap.org/img/wn/01d@2x.png",
-    "wind_speed": 5.1,
-    "wind_deg": 60,
-    "gust": 5.59,
-<<<<<<< HEAD
+    "icon": "https://openweathermap.org/img/wn/01n@2x.png",
+    "wind_speed": 4.32,
+    "wind_deg": 52,
+    "gust": 8.93,
     "sunrise": "01:13 AM",
     "sunset": "11:22 AM"
-=======
-    "sunrise": "01:13 PM",
-    "sunset": "11:22 PM"
->>>>>>> Toledo-Bend/main
   }
 };
