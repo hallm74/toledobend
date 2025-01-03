3907f1cfb2c269f84a0f8678a698399eb58ef8ac
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-03 13:45:35 -0600

    changes to source

--- data.js
+++ data.js
@@ -1,49 +1,14 @@
 const data = {
   "lakeLevel": "169.69 ft",
   "weather": {
-    "coord": {
-      "lon": -93.4841,
-      "lat": 31.5688
-    },
-    "weather": [
-      {
-        "id": 800,
-        "main": "Clear",
-        "description": "clear sky",
-        "icon": "01d"
-      }
-    ],
-    "base": "stations",
-    "main": {
-      "temp": 64.72,
-      "feels_like": 63.43,
-      "temp_min": 64.72,
-      "temp_max": 64.72,
-      "pressure": 1027,
-      "humidity": 54,
-      "sea_level": 1027,
-      "grnd_level": 1016
-    },
-    "visibility": 10000,
-    "wind": {
-      "speed": 5.55,
-      "deg": 60,
-      "gust": 6.6
-    },
-    "clouds": {
-      "all": 0
-    },
-    "dt": 1735931883,
-    "sys": {
-      "type": 1,
-      "id": 3311,
-      "country": "US",
-      "sunrise": 1735910031,
-      "sunset": 1735946578
-    },
-    "timezone": -21600,
-    "id": 4332518,
-    "name": "Many",
-    "cod": 200
+    "temp": 64.72,
+    "feels_like": 63.39,
+    "description": "clear sky",
+    "icon": "https://openweathermap.org/img/wn/01d@2x.png",
+    "wind_speed": 5.1,
+    "wind_deg": 60,
+    "gust": 5.59,
+    "sunrise": "07:13 AM",
+    "sunset": "05:22 PM"
   }
 };
