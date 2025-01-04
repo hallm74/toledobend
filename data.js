12fc7c7f9a7380579143f4358e9bb16f8dfccd2a
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-04 11:00:05 -0600

    changes

--- data.js
+++ data.js
@@ -1,13 +1,12 @@
 const data = {
-  "lakeLevel": "169.62ft 10:00am",
+  "lakeLevel": "169.65ft 10:30am",
   "weather": {
     "temp": 52.12,
-    "feels_like": 50.41,
+    "feels_like": 50.38,
     "description": "overcast clouds",
-    "wind_speed": 5.57,
-    "wind_deg": 115,
-    "gust": 18.88,
-<<<<<<< HEAD
+    "wind_speed": 6.35,
+    "wind_deg": 125,
+    "gust": 19.17,
     "sunrise": "01:13 AM",
     "sunset": "11:23 AM",
     "dayOrNight": "day"
@@ -15,10 +14,5 @@ const data = {
   "fishingReport": {
     "date": "Dec 31, 2024",
     "report": "FAIR. Water normal stain; 58 degrees; 2.40 feet below pool. After the recent rains most of all the creeks are muddy and with water still coming in. The shallow bite is going to be hard with the dirty water, but chartreuse or orange colored spinnerbaits and chatterbaits always work great in the mud. The deep fish are going to be slow due to the rising water, but casting crankbaits, dropshots and spoons should land catches.  Mid range fish are going to be best in 12-20 feet on Texas rigs, Carolina rigs and big crankbaits. There is a new moon coming this week which should get the female bass thinking about getting ready to make a move up. A cool down in the forecast with temperatures in the high 50s. No reports on crappie. Report by Stephen Johnston, Johnston Fishing."
-=======
-    "sunrise": "07:13 AM",
-    "sunset": "05:23 PM",
-    "dayOrNight": "day"
->>>>>>> Toledo-Bend/main
   }
 };
