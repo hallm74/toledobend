9d747ccd41e8ad6c546d98651ca8e25dc0c602d5
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-03 19:38:52 -0600

    added back opengraph

--- data.js
+++ data.js
@@ -2,18 +2,13 @@ const data = {
   "lakeLevel": "169.74 ft",
   "weather": {
     "temp": 52.12,
-    "feels_like": 50.85,
+    "feels_like": 50.7,
     "description": "clear sky",
-    "wind_speed": 5.55,
-    "wind_deg": 78,
-    "gust": 20.85,
-<<<<<<< HEAD
+    "wind_speed": 7.23,
+    "wind_deg": 90,
+    "gust": 22.95,
     "sunrise": "01:13 AM",
     "sunset": "11:22 AM",
-=======
-    "sunrise": "07:13 AM",
-    "sunset": "05:22 PM",
->>>>>>> Toledo-Bend/main
     "dayOrNight": "night"
   }
 };
