45e5b21eba3358942a5c1228c0ba4e6bf5ad7b23
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-03 18:32:34 -0600

    trying to add a night mode

--- data.js
+++ data.js
@@ -2,12 +2,12 @@ const data = {
   "lakeLevel": "169.74 ft",
   "weather": {
     "temp": 53.92,
-    "feels_like": 52.77,
+    "feels_like": 52.83,
     "description": "clear sky",
     "icon": "https://openweathermap.org/img/wn/01n@2x.png",
-    "wind_speed": 4.59,
-    "wind_deg": 64,
-    "gust": 9.82,
+    "wind_speed": 5.55,
+    "wind_deg": 78,
+    "gust": 20.85,
     "sunrise": "01:13 AM",
     "sunset": "11:22 AM"
   }
