603fbaca4ddbad57d42ded9c97f5d286d8aca661
hallm74 <68484824+hallm74@users.noreply.github.com>
2025-01-08 18:31:45 -0600

    last 6 barametric pressure

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 const data = {
   "lakeLevel": "169.43ft 5:30pm",
   "currentWeather": {
-    "temp": 39,
-    "feels_like": 35.42,
+    "temp": 38.14,
+    "feels_like": 34.14,
     "description": "overcast clouds",
-    "wind_speed": 4.85,
-    "wind_deg": 11,
-    "gust": 7.81,
-    "sunrise": "01:13 AM",
-    "sunset": "11:28 AM",
+    "wind_speed": 5.19,
+    "wind_deg": 6,
+    "gust": 8.81,
+    "sunrise": "7:13:43 AM",
+    "sunset": "5:28:17 PM",
     "dayOrNight": "night",
-    "humidity": 56,
+    "humidity": 59,
     "uv_index": 0,
     "pressure": 1029,
     "moon_phase": 0.31
@@ -83,6 +83,14 @@ const data = {
     }
   ],
   "weatherAlerts": [],
+  "barometricPressureHistory": [
+    1029,
+    1029,
+    1029,
+    1029,
+    1029,
+    1028
+  ],
   "fishingReport": {
     "date": "Jan 8, 2025",
     "report": "FAIR. Water normal stain; 58 degrees; 2.49 feet below pool. After the recent rains most of all the creeks are muddy and with water still coming in. The shallow bite is going to be hard with the dirty water, but chartreuse or orange colored spinnerbaits and chatterbaits always work great in the mud. The deep fish are going to be slow due to the rising water, but casting crankbaits, dropshots and spoons should land catches.  Mid range fish are going to be best in 12-20 feet on Texas rigs, Carolina rigs and big crankbaits. There is a new moon coming this week which should get the female bass thinking about getting ready to make a move up. A cool down in the forecast with temperatures in the high 50s. No reports on crappie. Report by Stephen Johnston, Johnston Fishing."
